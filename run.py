12b67b6b9f299032897d55934cdbf4343fdb9e28
Luke-N-G <lucas.gutierrez@ib.edu.ar>
2024-09-19 17:25:14 -0300

    pcGNLSE pbar, sensor usage

--- run.py
+++ run.py
@@ -6,11 +6,11 @@ Created on Fri Jun 30 10:43:33 2023
 """
 #Comentario
 
-import numpy as np
-from solvers.solvegnlse import SolveNLS
-from common.commonfunc import SuperGauss, Soliton, Sim, Fibra, Pot, fftshift, FT, IFT
-from common.plotter import plotinst, plotcmap, plt
 
+import numpy as np
+from common.commonfunc import SuperGauss, Two_Pulse, Soliton, Sim, Fibra, Pot, fftshift, FT, IFT
+from common.plotter import plotinst, plotcmap, plt
+from solvers.solvegnlse import SolveNLS
 
 #Parametros para la simulación
 N = int(2**14)  #puntos
@@ -22,18 +22,18 @@ lambda0 = 1550                       #Longitud de onda central (nm)
 omega0  = 2*np.pi*c/lambda0
 
 #Parametros para la fibra
-L     = 400                       #Lfib:   m
-b2    = -20e-3                    #Beta2:  ps^2/m
-b3    = 0                         #Beta3:  ps^3/m
-gam   = 1.4e-3                    #Gamma:  1/Wm
+L     = 12.5*5                       #Lfib:   m
+b2    = -20e-3*-1                    #Beta2:  ps^2/m
+b3    = 10e-3                         #Beta3:  ps^3/m
+gam   = 0#1.4e-3                    #Gamma:  1/Wm
 gam1  = gam/omega0*0              #Gamma1: 0
 alph  = 0                         #alpha:  1/m
 TR    = 3e-3*0                    #TR:     fs
-fR    = 0.18*1                    #fR:     adimensional (0.18)
+fR    = 0.18*0                    #fR:     adimensional (0.18)
 
 #Parámetros pulso gaussiano:
 amp    = 1                 #Amplitud:  sqrt(W), Po = amp**2
-ancho  = .4                  #Ancho T0:  ps
+ancho  = .5                  #Ancho T0:  ps
 offset = 0                  #Offset:    ps
 chirp  = 0                  #Chirp:     1/m
 orden  = 1                  #Orden
@@ -53,7 +53,7 @@ soliton = Soliton(sim.tiempo, ancho, fibra.beta2, fibra.gamma, orden = 1)
 
 #%% Corriendo la simulación
 
-zlocs, AW, AT = SolveNLS(sim, fibra, soliton, raman=True, z_locs=100)
+zlocs, AW, AT = SolveNLS(sim, fibra, pulso, raman=False, z_locs=100)
 
 
 #%% Plotting
@@ -63,3 +63,9 @@ plotcmap(sim, fibra, zlocs, AT, AW, Wlim=[-1,1], Tlim=[-7,7])
 
 
 #%% Extra
+
+plt.plot(sim.tiempo/ancho, Pot(AT)[0]/np.max(Pot(AT)[0]))
+plt.plot(sim.tiempo/ancho, Pot(AT)[-1]/np.max(Pot(AT)[0]))
+plt.xlim([-6,16])
+plt.ylim([0,1.1])
+plt.show()
--- solvers/solvegnlse.py
+++ solvers/solvegnlse.py
@@ -9,6 +9,7 @@ from common.commonfunc import FT, IFT, fftshift, Pot, Sim, Fibra
 import numpy as np
 from scipy.integrate import solve_ivp  #Para resolver eq. diferenciales ordinarias
 from functools import partial          #Permite evaluar parcialmente una función g(x,y) = partial(f(x,y,z), z = 2)
+from tqdm import tqdm
 
 
 '''Función dB/dz = f(B,z)
@@ -37,7 +38,84 @@ def Raman(T, tau1=12.2e-3, tau2=32e-3, fR=0.18): #Agrawal pag.38: t1 = 12.2s fs,
     R_W = fR * hR_W + (1-fR)*np.ones( len(T) )
     return R_W
 
-def SolveNLS(sim: Sim, fib: Fibra, pulso_0, raman=False, z_locs=None):
+
+def SolveNLS(sim: Sim, fib: Fibra, pulso_0, raman=False, z_locs=None, pbar=True):
+
+    #Calculamos el espectro inicial, es lo que vamos a evolucionar.
+    espectro_0 = FT(pulso_0)
+
+    #Calculamos el operador lineal
+    D_w = 1j * fib.beta2/2 * (2*np.pi*sim.freq)**2 + 1j * fib.beta3/6 * (2*np.pi*sim.freq)**3 - fib.alpha/2
+    D_w = np.array(D_w)
+    
+    if fib.betas:
+        D_w = 0
+        for i in range(len(fib.betas)):
+            D_w = D_w + 1j*fib.betas[i]/np.math.factorial(i+2) * (2*np.pi*sim.freq)**(i+2)
+        D_w = np.array(D_w)
+
+    #Introducimos todos los parametros en la función, quedando f(z, B) = dB/dz
+    if raman:
+      RW = Raman(sim.tiempo, fR = fib.fR)
+      f_B = partial(dBdz_raman, D = D_w, w = 2*np.pi*sim.freq, gamma = fib.gamma, gamma1 = fib.gamma1, RW = RW)
+    else:
+      f_B = partial(dBdz, D = D_w, w = 2*np.pi*sim.freq, gamma = fib.gamma, TR = fib.TR)
+
+    #Tolerancias para integrar (Tolerancias estandar: rtol=1e-5, atol=1e-8)
+    rtol = 1e-3
+    atol = 1e-6
+
+    if pbar:  # Por si queremos la barra de progreso
+        with tqdm(total=fib.L, unit="m") as pbar:
+            if raman:
+                def dBdz_with_progress(z, B):
+                    pbar.update(abs(z - dBdz_with_progress.prev_z))
+                    dBdz_with_progress.prev_z = z
+                    return dBdz_raman(z, B, D = D_w, w = 2*np.pi*sim.freq, gamma = fib.gamma, RW = RW)
+                dBdz_with_progress.prev_z = 0
+            else:
+                def dBdz_with_progress(z, B):
+                    pbar.update(abs(z - dBdz_with_progress.prev_z))
+                    dBdz_with_progress.prev_z = z
+                    return dBdz(z, B, D = D_w, w = 2*np.pi*sim.freq, gamma = fib.gamma, TR = fib.TR)
+                dBdz_with_progress.prev_z = 0
+
+            # Usamos solve_ivp: Buscamos solución entre 0 y L
+            if z_locs:  # Si le pasamos un valor de z_locs: El array de salida tendra z_locs elementos
+                t_eval = np.linspace(0, fib.L, z_locs)
+                sol = solve_ivp(dBdz_with_progress, [0, fib.L], y0=espectro_0, rtol=rtol, atol=atol, t_eval=t_eval)
+            else:
+                sol = solve_ivp(dBdz_with_progress, [0, fib.L], y0=espectro_0, rtol=rtol, atol=atol)
+    else:  # Sin la barra de progreso
+        if z_locs:  # Si le pasamos un valor de z_locs: El array de salida tendra z_locs elementos
+            t_eval = np.linspace(0, fib.L, z_locs)
+            sol = solve_ivp(f_B, [0, fib.L], y0=espectro_0, rtol=rtol, atol=atol, t_eval=t_eval)
+        else:
+            sol = solve_ivp(f_B, [0, fib.L], y0=espectro_0, rtol=rtol, atol=atol)
+
+    zlocs = sol["t"]  #Puntos de z donde tenemos B(w,z)
+    ysol  = sol["y"]  #Array, en cada elemento se tiene un subarray [B(w0,z0), B(w0,z1), ..., B(w0,zf)]
+    
+
+    zlocs = sol["t"]  #Puntos de z donde tenemos B(w,z)
+    ysol  = sol["y"]  #Array, en cada elemento se tiene un subarray [B(w0,z0), B(w0,z1), ..., B(w0,zf)]
+    print(sol["message"])
+
+    ysol  = np.array(ysol)
+    ysol_transposed = ysol.T
+    A_w = ysol_transposed
+    for j in range( len(zlocs) ):
+        A_w[j,:] = A_w[j,:] * np.exp(D_w * zlocs[j])
+    A_t = np.array([IFT(a_w) for a_w in A_w], dtype=complex)
+
+    return zlocs, A_w, A_t #Nos devuelve: zlocs = Posiciones donde calculamos la solución, A_w = Matriz con la evolución del espectro, A_t = Matriz con la evolución del pulso
+
+
+
+
+#%% Legacy
+
+def SolveNLS_legacy(sim: Sim, fib: Fibra, pulso_0, raman=False, z_locs=None):
 
     #Calculamos el espectro inicial, es lo que vamos a evolucionar.
     espectro_0 = FT(pulso_0)
@@ -70,7 +148,7 @@ def SolveNLS(sim: Sim, fib: Fibra, pulso_0, raman=False, z_locs=None):
         sol = solve_ivp(f_B, [0, fib.L], y0 = espectro_0, rtol = rtol, atol = atol, t_eval=t_eval)
     else:
         sol = solve_ivp(f_B, [0, fib.L], y0 = espectro_0, rtol = rtol, atol = atol)
-
+        
     zlocs = sol["t"]  #Puntos de z donde tenemos B(w,z)
     ysol  = sol["y"]  #Array, en cada elemento se tiene un subarray [B(w0,z0), B(w0,z1), ..., B(w0,zf)]
     print(sol["message"])
